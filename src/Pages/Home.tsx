21742aa533166753cd58809002799f48fb3094e2
LouisWackenier <Louis.wackenier@gmail.com>
2023-08-22 09:03:35 +0200

    Removed the circle effect

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+
 
 const Section = styled.div`
      height: 100vh
-     scroll-snap-align: center;
+     
 `;
 
 const HeroSection = styled.div`
@@ -26,55 +26,13 @@ const TitleSubText = styled.div`
     font-size: large;
 `;
 
-// Define the breathing animation
-const breathingAnimation = keyframes`
-  0% {
-    transform: scale(1);
-    opacity: 0.8;
-  }
-  50% {
-    transform: scale(1.05);
-    opacity: 1;
-  }
-  100% {
-    transform: scale(1);
-    opacity: 0.8;
-  }
-`;
-
-// Styled component with the breathing animation
-const BackgroundContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BreathingSection = styled.section`
-  position: absolute;
-  width: 300px;
-  height: 300px;
-  background: radial-gradient(ellipse at center, rgba(255, 255, 255, 0.8) 0%, transparent 80%);
-  border-radius: 50%;
-  margin-top: 30%;
-  left: -1;
-  margin-right: 100%;
-  animation: ${breathingAnimation} 4s infinite;
-`;
-
-
-
 const Home = () => {
 
   return (
      <Section>
          <HeroSection>
-            <BackgroundContainer>
-                <BreathingSection></BreathingSection>
-            </BackgroundContainer>
              <WelcomeTitle>Welcome to my website</WelcomeTitle>    
              <TitleSubText>My name is Louis and I am a fullstack developer</TitleSubText>
-            
          </HeroSection>
      </Section>
   
