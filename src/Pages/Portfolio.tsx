c578606c423cd09baaeb5fdf66eb32cd64d3558a
LouisWackenier <Louis.wackenier@gmail.com>
2024-01-30 08:43:52 +0100

    Working on the portfolio

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -7,12 +7,12 @@ import krypt from '../assets/images/ProjectImages/Blockchain.png';
 import { Link } from 'react-router-dom';
 
 const Section = styled.div`
-     height: 100vh;
+     height: 110vh;
 `;
 
 const PortfolioTitle = styled.h1`
      margin-top: 100px;
-     margin-left: 35%;
+     text-align: center;
      animation: textGlow 5s infinite alternate;
 
      @keyframes textGlow {
@@ -31,26 +31,39 @@ const PortfolioTitle = styled.h1`
 `;
 
 const BreakLine = styled.div`
-      margin-left: 24%;
+      margin-left: 25%;
       width: 50%;
       height: 2px; /* Set the height of the separate line */
       background-color: rgba(255, 255, 255, 0.5); /* Color of the separate line */
 `;
 
 const TitleSubText = styled.h2`
-    margin-left: 27%;
+    text-align: center;
 `;
 
 const Container = styled.div`
+          border-radius: 2vh;
           margin-top: 5%;
           margin-left: 30%;
+          width: 27vw;
+          padding-top: 1vh;
+          padding-left: 1vh;
+          padding-right: 1vh;
+          height: 43vh;
           justify-content: flex-start;
+          z-index: -1;
+          background-color: #4d4747;
+          transition: background-color 2s;
+
+          &:hover {
+           background-color: #ffffff;
+           color: black;
+           box-shadow: darkblue;
+  }
 `;
 
 const Image = styled.img`
      width: 500px;
-     display: flex;   
-     border-radius: 10px;
      border: solid darkgray 2px;
 `;
 
@@ -58,13 +71,15 @@ const ParagraphTitle = styled.div`
      font-size: 30px;
      display: flex;
 
+     background-color: darkgray;
+
      animation: textGlow 5s infinite alternate;
 `;
 
 const TextParagraph = styled.div`
+     background-color: darkgray;
+     display: flex;
      font-size: 16px;
-     width: 400px;
-     margin: 10px;
 `;
 
 const TextContainer = styled.div`
@@ -74,18 +89,18 @@ const TextContainer = styled.div`
 `;
 
 const VisitButton = styled.button`
+     z-index: 1;
      font-weight: 700;
      margin-top: 1vh;
      font-size: large;
      cursor: pointer;
 
      border: 2px solid white;
-     background-color: transparent;
+     background-color: black;
      letter-spacing: 3px;
      border-radius: 5px;
      height: 50px;
      width: 175px;
-     margin-left: 2vw;
 
      transition: background-color 0.5s;
 
@@ -98,17 +113,18 @@ const VisitButton = styled.button`
 const Portfolio = () => {
   return ( 
     <Section id="Portfolio">    
-         <PortfolioTitle>Welcome to my Portfolio section</PortfolioTitle> 
-         <TitleSubText>Here you can view all my latest projects and other's that I have built</TitleSubText>
+         <PortfolioTitle>Welcome to my Portfolio</PortfolioTitle> 
+         <TitleSubText>Here you can view all my latest projects that I have built</TitleSubText>
         <BreakLine />
 
         {/*Project 1*/}
          <Container>
+            <Image src={krypt}></Image>
             <TextContainer>
                <ParagraphTitle>Blockchain App</ParagraphTitle>
                <TextParagraph>A web app built with tailwindCSS. It is a crypto wallet web app</TextParagraph>
              </TextContainer>
-                <Image src={krypt}></Image>
+                
              <Link to="https://github.com/LouisWackenier?tab=repositories">
                <VisitButton>View Project</VisitButton>
                <VisitButton>Github Repo</VisitButton>
